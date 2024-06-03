4894eb6fb2334ce87d4aa31276124cf82ff2f5d9
juho Kim <42205951+gabjuho@users.noreply.github.com>
2024-06-03 23:10:47 +0900

    :bug: 레시피에서 빈 데이터가 하얀 UI로 보여지는 버그 수정

--- Assets/Dev/Feature/HologramUI/CocktailTypeTable.cs
+++ Assets/Dev/Feature/HologramUI/CocktailTypeTable.cs
@@ -4,8 +4,6 @@ using System.Collections.Generic;
 using System.Linq;
 using JetBrains.Annotations;
 using UnityEditor;
-using UnityEngine;
-using Object = UnityEngine.Object;
 
 public class CocktailTypeTable
 {
